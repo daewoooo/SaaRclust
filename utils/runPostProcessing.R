bf2247d764d8bef94278ae29c3f757f8bd5a5249
maryamghr <maryam.ghareghani@gmail.com>
2018-03-20 13:53:26 +0100

    Update runPostProcessing.R

--- utils/runPostProcessing.R
+++ utils/runPostProcessing.R
@@ -18,7 +18,7 @@ thresholds <- c(0, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99)
 message("Venn diagram statistics...")
 venn.stats <- VennDiagramStats(inputfolder = inputfolder, thresholds = thresholds)
 venn <- file.path(outputfolder, "VennStatistics.txt")
-write.table(venn.stats, file = venn, sep = "\t")
+write.table(venn.stats, file = venn, sep = "\t", quote = F)
 
 message("Preparing clustering accuracy lollipop plot ...")
 ClustersAccuracyPerChrPerDir(inputfolder=inputfolder, thresholds=thresholds, minLib=5) -> accplt.obj
