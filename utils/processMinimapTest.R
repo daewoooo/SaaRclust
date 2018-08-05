6e9d3ecd3500ccd7f8e88561cd45a5b2824f5155
daewoooo <porubsky@lap-13-72.mpi-inf.mpg.de>
2018-08-05 10:36:47 -0700

    fixed bugs in old function dependencies

--- utils/processMinimapTest.R
+++ utils/processMinimapTest.R
@@ -1,8 +1,11 @@
+## Load required libraries
 library("gsubfn")
 library("data.table")
 library("RColorBrewer")
+library("SaaRclust")
 
-path <-  "/media/daewoooo/WORK/SS2PacBio_alignment_HG00733/Test_minimap/minimap_test/" 
+## Path to the test minimap folder to be processed
+path <- "/home/porubsky/WORK/PROJECTS/SaaRclust_project/TEST_minimap/aligns_50000reads" 
 files2process <- list.files(path = path, pattern = '\\.log')
 
 results <- list()
@@ -31,31 +34,23 @@ for (file in files2process) {
 
   minimap.file <- gsub(file, pattern = '\\.log', replacement = '')
   minimap.file.path <- file.path(path, minimap.file)
-  tab.in <- importTestData(infile = minimap.file.path)
+  tab.in <- importData(infile = minimap.file.path)
 
-  #export data quality measures
+  ## Export data quality measures
   qual.measures <- getQualMeasure(tab.in)
-  mapp.stat.counts <- qual.measures$mapp.stat.counts
-  mapp.gaps.tab <- qual.measures$mapp.gaps.stat
-  
-  #qual.plt <- plotQualMeasure(qual.measures)
-  #plt.file <- gsub(file, pattern = '\\.log', replacement = '\\_plot.pdf')
-  #plt.file <- file.path(path, plt.file)
-  #ggsave(filename = plt.file, plot = qual.plt, width = 10, height = 10)
 
-  read.map.dist.mean <- round(mean(mapp.stat.counts$SSread.perPB))
-  SSlib.perPB.mean <- round(mean(mapp.stat.counts$SSlib.perPB))
-  gaps.perSS.mean <- round(mean(mapp.gaps.tab$matchWithgap))
-  accur.counts <- table(mapp.gaps.tab$mapp.accur)
+  mean.SSreads.perPB <- round(mean(qual.measures$SSreads.perPB))
+  mean.SSlib.perPB <- round(mean(qual.measures$SSlib.perPB$counts))
+  mean.SSreads.perlib.perPB <- round(mean(qual.measures$SSreads.perlib.perPB))
+  accur.counts <- table(tab.in$SSchrom == tab.in$PBchrom)
   falses <- round((accur.counts[1]/sum(accur.counts))*100, digits = 2)
   trues <- round((accur.counts[2]/sum(accur.counts))*100, digits = 2)
 
   total.PBreads <- length(unique(tab.in$PBreadNames))
   total.SSreads <- length(unique(tab.in$SSreadNames))
 
-  v <- c(k=as.numeric(k), w=as.numeric(w), L=as.numeric(L), f=as.numeric(f), num.minimizers=as.numeric(minimizers), real.time=as.numeric(real.time), cpu.time=as.numeric(cpu.time), mean.SSread.perPB=read.map.dist.mean, mean.SSlib.perPB=SSlib.perPB.mean, gaps.perSS.mean=gaps.perSS.mean, total.PBreads=as.numeric(total.PBreads), total.SSreads=as.numeric(total.SSreads), falses=falses, trues=trues)
+  v <- c(k=as.numeric(k), w=as.numeric(w), L=as.numeric(L), f=as.numeric(f), num.minimizers=as.numeric(minimizers), real.time=as.numeric(real.time), cpu.time=as.numeric(cpu.time), mean.SSreads.perPB=mean.SSreads.perPB, mean.SSlib.perPB=mean.SSlib.perPB, mean.SSreads.perlib.perPB=mean.SSreads.perlib.perPB, total.PBreads=as.numeric(total.PBreads), total.SSreads=as.numeric(total.SSreads), falses=falses, trues=trues)
   results[[file]] <- v
-  #plots[[file]] <- qual.plt
 }
 
 final.tab <- do.call(rbind,results)
@@ -65,15 +60,13 @@ write.table(x=final.tab, file = fileName, quote = F)
 
 save(plots, file = "plots_minimap_test.RData")
 
-#final.tab.long <- melt(final.tab, id.vars=c("k","w","L","f"), measure.vars=c("num.minimizers","real.time","cpu.time","mean.SSread.perPB","mean.SSlib.perPB","gaps.perSS.mean","total.PBreads","total.SSreads","falses.FALSE","trues.TRUE"))
-final.tab.long <- melt(final.tab, id.vars=c("k","w","L","f"), measure.vars=c("num.minimizers","real.time","mean.SSread.perPB","mean.SSlib.perPB","gaps.perSS.mean","total.PBreads","total.SSreads","falses.FALSE","trues.TRUE"))
+## Prepare table for plotting
+final.tab.long <- melt(final.tab, id.vars=c("k","w","L","f"), measure.vars=c("num.minimizers","real.time","mean.SSreads.perPB","mean.SSlib.perPB","mean.SSreads.perlib.perPB","total.PBreads","total.SSreads","falses.FALSE","trues.TRUE"))
 
-ggplot(final.tab, aes(x=f, y=trues.TRUE)) + geom_line() + geom_point() + facet_grid(w ~ .)
-
-final.tab.long$value[is.na(final.tab.long$value)] <- 0
-ggplot(final.tab.long, aes(x=f, y=value, group=variable, color=variable)) + geom_line() + geom_point() + facet_grid(variable ~ w + k, scales="free_y") + scale_color_manual(values = brewer.pal(n=9, name="Set1"))
-
-ggplot(final.tab.long[final.tab.long$k != 11,], aes(x=f, y=value, group=variable, color=variable)) + geom_line() + geom_point() + facet_grid(variable ~ w + k, scales="free_y") + scale_color_manual(values = brewer.pal(n=9, name="Set1"))
-
-ggplot(final.tab.long[final.tab.long$k != 11 & final.tab.long$f != 0.5,], aes(x='', y=value, group=variable, color=variable)) + geom_line() + geom_point() + facet_grid(variable ~ w + k + f, scales="free_y") + scale_color_manual(values = brewer.pal(n=9, name="Set1"))
+## Prepare plot
+plt <- ggplot(final.tab.long, aes(x='', y=value, color=variable))
+plt <- plt + geom_point(size=2) + facet_grid(variable ~ L + k + f, scales="free_y", labeller = labeller(.cols = label_both))
+plt <- plt + scale_color_manual(values = brewer.pal(n=9, name="Set1"), guide="none")
+plt <- plt + theme(strip.text.y = element_text(angle = 360))
+plt <- plt + xlab("")
 
