35df3fd3c419cdf9fec8dc170de56a836fce0502
Maryam Ghareghani <mgharegh@d3compute02.mpi-inf.mpg.de>
2018-08-16 20:04:24 +0200

    added a simple algorithm for pairing clusters

--- R/findClusterPartners.R
+++ R/findClusterPartners.R
@@ -63,6 +63,47 @@ findClusterPartners2 <- function(theta.param=NULL) {
   return(max.match$matching)
 }
 
+#' Get pairs of clusters coming from the same chromosome with different directionalities
+#'
+#' This function finds the pairs by computing the pairwise distances between clusters theta_wc parameters, and pais each cluster with the cluster with min dist
+#' Note that the first 23 minimum values should form a matching in the complete graph of clusters (this condition is observed to be held in practice)
+#' TODO: We should replace this simple function by a proper max matching algorithm or some other solution that properly solves this problem and finds non-overlappinh pairs of clusters
+#' 
+#' @param theta.param A \code{list} containing estimated cell types per cluster for each cell.
+#' @return A \code{vector} of pairs of clusters IDs that belong to the same chromosome.
+#' @importFrom maxmatching maxmatching
+#' @importFrom igraph graph E
+#' @author Maryam Ghareghani
+#' @export
+
+findClusterPartners_simple <- function(theta.param=NULL) {
+  
+  # get only wc thetas
+  theta.param.wc <- lapply(theta.param, function(x) x[,3])
+  # cbid wc thetas for all single cells
+  all.theta.param.wc <- do.call(cbind, theta.param.wc)
+  # compute the pairwise distance of all wc thetas (all pairs of clusters)
+  d <- as.matrix(dist(all.theta.param.wc))
+  # compute the min index in each row (excluding the diogonal element)
+  min.cost.clust.pair <- sapply(1:nrow(d), function(i) return(which.min(d[i,-i])))
+  min.cost.clust.pair <- data.table(first_clust=1:nrow(d), second_clust=as.numeric(names(min.cost.clust.pair)))
+  # put the min rank cluster in the first column
+  min.cost.clust.pair[, min_id_clust:=min(first_clust, second_clust), by=1:nrow(min.cost.clust.pair)]
+  min.cost.clust.pair[, second_clust:=max(first_clust, second_clust), by=1:nrow(min.cost.clust.pair)]
+  min.cost.clust.pair[, `:=`(first_clust=min_id_clust, min_id_clust=NULL)]
+  
+  # compute the frequencies of each cluster in this pairing
+  fr <- table(c(min.cost.clust.pair$first_clust, min.cost.clust.pair$second_clust))
+  
+  # check whether the pairing is a matching
+  assert_that(all(sort(unique(fr))==1:3) & length(which(duplicated(min.cost.clust.pair)))==floor(nrow(min.cost.clust.pair)/2)) %>% invisible()
+  
+  # keep only the duplicated rows (keep the matching and kick out the garbage cluster)
+  min.cost.clust.pair <- min.cost.clust.pair[duplicated(min.cost.clust.pair)]
+  
+  return(min.cost.clust.pair)
+}
+
 
 #' Get pairs of clusters coming from the same chromosome
 #'
