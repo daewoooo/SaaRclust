1a2f20139298245c867fb14c030ca4b0521a9458
daewoooo <porubsky@eee-102737.(none)>
2018-09-26 15:21:46 -0700

    updated function documentation

--- R/dumped_functions.R
+++ R/dumped_functions.R
@@ -236,3 +236,66 @@ mergeSplitedClusters <- function(cluster2merge=NULL, soft.pVal=NULL) {
   
   return(do.call(cbind,merged.pVals))
 } 
+
+
+#' Get pairs of clusters coming from the same chromosome with different directionalities
+#'
+#' This function solves the Maximum matching problem for all possible pairs of clusters and reports pairs with highest similarity.
+#'
+#' @param theta.param A \code{list} containing estimated cell types per cluster for each cell.
+#' @return A \code{vector} of pairs of clusters IDs that belong to the same chromosome.
+#' @importFrom maxmatching maxmatching
+#' @importFrom igraph graph E
+#' @author Maryam Ghareghani, David Porubsky
+#' @export
+
+findClusterPartners2 <- function(theta.param=NULL) {
+  
+  # get only wc thetas
+  theta.param.wc <- lapply(theta.param, function(x) x[,3])
+  # cbid wc thetas for all single cells
+  all.theta.param.wc <- do.call(cbind, theta.param.wc)
+  # compute the pairwise distance of all clusters wc thetas
+  d <- as.matrix(dist(all.theta.param.wc))
+  # convert distance to a similarity measure
+  d <- max(d) - d
+  for (i in 1:nrow(d)){d[i,i] <- 0}
+  G <- igraph::graph.adjacency(d, mode="undirected", weighted=TRUE)
+  #TODO: replce the following code by another package or code (maxmatching doesn't work)
+  max.match <- maxmatching::maxmatching(G, weighted = TRUE)
+  
+  return(max.match$matching)
+}
+
+#' Get pairs of clusters coming from the same chromosome
+#'
+#' This function finds cluster coming from the same chromosome and having the same directionality
+#'
+#' @param theta.param A \code{list} of estimated cell types for each cluster and each cell.
+#' @return A \code{vector} of pairs of clusters IDs that belong to the same chromosome.
+#' @importFrom igraph graph max_cliques
+#' @author David Porubsky
+#' @export
+
+findSplitedClusters <- function(theta.param=NULL) {
+  
+  euc.dist.v <- function(v) sqrt(sum((v[1] - v[2]) ^ 2))
+  
+  pairwise.dist <- list()
+  for (i in 1:length(theta.param)) {
+    cell.theta <- theta.param[[i]]
+    pairs <- t(combn(nrow(cell.theta), 2))
+    pairs.w <- cbind( cell.theta[pairs[,1],1], cell.theta[pairs[,2],1] )
+    dist <- apply(pairs.w, 1, euc.dist.v)
+    pairwise.dist[[i]] <- dist
+  }
+  pairwise.dist.m <- do.call(cbind, pairwise.dist)
+  pairwise.simil.m <- max(pairwise.dist.m)-pairwise.dist.m
+  simil.sum <- rowSums(pairwise.simil.m)
+  zscores <- (simil.sum - mean(simil.sum)) / sd(simil.sum)
+  idx <- zscores > 3.291 #99.9 confidence interval
+  G <- igraph::graph(c(rbind(pairs[idx,1], pairs[idx,2])), directed = FALSE)
+  sub.G <- igraph::max_cliques(G, min = 2)
+  
+  return(sub.G)
+}
