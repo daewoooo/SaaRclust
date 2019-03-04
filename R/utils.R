3753900fb5d09af72c53c731d6550b6c362a8863
daewoooo <porubsky@eee-102737.(none)>
2019-03-03 16:17:56 -0800

    CollapseBin function now can sum user defined values in collapsed regions

--- R/utils.R
+++ R/utils.R
@@ -8,15 +8,25 @@
 #' @author David Porubsky
 #' @export
 #' 
-collapseBins <- function(gr, id.field=0) {
+collapseBins <- function(gr, id.field=0, measure.field=NULL) {
   ## Include seqnames into the unique ID
   unique.ID <- paste0(seqnames(gr), '_', mcols(gr)[,id.field])
   ## Get continous runs of the same unique ID
-  ind.last <- cumsum(runLength(Rle(unique.ID))) ##get indices of last range in a consecutive(RLE) run of the same value
-  ind.first <- c(1,cumsum(runLength(Rle(unique.ID))) + 1) ##get indices of first range in a consecutive(RLE) run of the same value
+  unique.ID.runLength <- runLength(Rle(unique.ID))
+  ind.last <- cumsum(unique.ID.runLength) ##get indices of last range in a consecutive(RLE) run of the same value
+  ind.first <- c(1,cumsum(unique.ID.runLength) + 1) ##get indices of first range in a consecutive(RLE) run of the same value
   ind.first <- ind.first[-length(ind.first)]  ##erase last index from first range indices 
   collapsed.gr <- GenomicRanges::GRanges(seqnames=seqnames(gr[ind.first]), ranges=IRanges(start=start(gr[ind.first]), end=end(gr[ind.last])), mcols=mcols(gr[ind.first]))
   names(mcols(collapsed.gr)) <- names(mcols(gr[ind.first]))
+  ## Sum values in user defined measure fields(s)
+  if (!is.null(measure.field)) {
+    run.ID  <- factor(rep(seq_along(unique.ID.runLength), unique.ID.runLength))
+    for (field in measure.field) {
+      mcols(collapsed.gr)[,field] <-  tapply(mcols(gr)[,field], run.ID, sum)
+      #collapsed.gr$C <-  tapply(gr$C, run.ID, sum)
+      #collapsed.gr$W <-  tapply(gr$W, run.ID, sum)
+    }
+  }
   return(collapsed.gr)
 }
 
