d83e28faaaa270caa7f921f9ddcdb215ea0af666
daewoooo <porubsky@eee-102737.(none)>
2019-09-13 11:45:17 -0700

    Added new function to export clustred FASTA assembly

--- R/exportPseudoChromosomalScaffolds.R
+++ R/exportPseudoChromosomalScaffolds.R
@@ -8,11 +8,12 @@
 #' order - contig order and ID - contig assignment to clusters.
 #' @param assembly.fasta An assembly FASTA file to extract DNA sequence determined by clustered.gr.
 #' @param outputfolder A path to a folder to export concatenated FASTA sequences.
+#' @param concat.fasta Set to \code{TRUE} if you want to concatenate FASTA sequences within a cluster by 100 N's.
 #' @importFrom Rsamtools indexFa FaFile scanFa
 #' @importFrom Biostrings reverseComplement DNAStringSet writeXStringSet
 #' @author David Porubsky
 #' @export
-exportPseudoChromosomalScaffolds <- function(clustered.gr=NULL, assembly.fasta=NULL, outputfolder=NULL) {
+exportPseudoChromosomalScaffolds <- function(clustered.gr=NULL, assembly.fasta=NULL, outputfolder=NULL, concat.fasta=TRUE) {
   ## Check if submitted fasta file is indexed
   assembly.fasta.idx <- paste0(assembly.fasta, ".fai")
   if (!file.exists(assembly.fasta.idx)) {
@@ -33,15 +34,20 @@ exportPseudoChromosomalScaffolds <- function(clustered.gr=NULL, assembly.fasta=N
     ## Reverse complement based on 'dir' field
     revcomp <- which(cluster.regions$dir == 'revcomp')
     cluster.seq[revcomp] <- Biostrings::reverseComplement(cluster.seq[revcomp])
-    ## Concatenate all sequences into a single FASTA separated by 100 N's.
-    delim <- paste(rep('N', 100), collapse = '')
-    cluster.seq.collapsed <- Biostrings::DNAStringSet(paste(cluster.seq, collapse = delim))
-    ## Create unique sequence name
-    #seq.name <- paste0(cluster.ID, '_', paste(names(cluster.seq), collapse = "."))
-    #names(cluster.seq.collapsed) <- seq.name
-    names(cluster.seq.collapsed) <- cluster.ID
-    ## Write final FASTA
-    destination <- file.path(outputfolder, paste0(cluster.ID, '.fasta')) 
-    Biostrings::writeXStringSet(x = cluster.seq.collapsed, filepath = destination, format = 'fasta')
+    if (concat.fasta) {
+      ## Concatenate all sequences into a single FASTA separated by 100 N's.
+      delim <- paste(rep('N', 100), collapse = '')
+      cluster.seq.collapsed <- Biostrings::DNAStringSet(paste(cluster.seq, collapse = delim))
+      names(cluster.seq.collapsed) <- cluster.ID
+      ## Write final FASTA
+      destination <- file.path(outputfolder, paste0(cluster.ID, '.fasta')) 
+      Biostrings::writeXStringSet(x = cluster.seq.collapsed, filepath = destination, format = 'fasta')
+    } else {
+      new.names <- paste0(names(cluster.seq), '_', cluster.regions$order, '_', cluster.regions$ID)
+      names(cluster.seq) <- new.names
+      ## Write final FASTA
+      destination <- file.path(outputfolder, paste0(cluster.ID, '.fasta')) 
+      Biostrings::writeXStringSet(x = cluster.seq, filepath = destination, format = 'fasta')
+    } 
   }
 }
