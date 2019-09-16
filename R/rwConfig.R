3513a0aeec57503b0afee97d9d7d43e4eae794b5
daewoooo <porubsky@eee-102737.(none)>
2019-09-16 08:52:15 -0700

    Added config file import/export

---
+++ R/rwConfig.R
@@ -0,0 +1,78 @@
+#' Read SaaRclust configuration file
+#'
+#' Read SaaRclust configuration file into a list structure. 
+#' The configuration file has to be specified in INI format. 
+#' R expressions can be used and will be evaluated.
+#'
+#' @param configfile Path to the configuration file
+#' @return A \code{list} with one entry for each element in \code{configfile}.
+#' @author Aaron Taudt, David Porubsky
+#' @importFrom utils read.table
+
+readConfig <- function(configfile) {
+
+    connection <- file(configfile) 
+    Lines  <- readLines(connection) 
+    close(connection) 
+
+    Lines <- chartr("[]", "==", Lines) # change section headers 
+    Lines <- gsub(" ", "", Lines) # no spaces
+
+    connection <- textConnection(Lines) 
+    data <- utils::read.table(connection, as.is = TRUE, sep = "=", fill = TRUE, quote="") 
+    close(connection) 
+    names(data) <- c('argument','value','section')
+
+    L <- data$argument == "" # location of section breaks 
+    data$section <- data$value[which(L)[cumsum(L)]]
+    data <- data[data$argument!="",]
+
+    configlist <- list() 
+    ToParse <- paste0("configlist$", data$argument, " <- ", data$value)
+#   ToParse  <- paste0("configlist$", data$section, "$",  data$argument, " <- ", data$value) # with sections
+
+    eval(parse(text=ToParse)) 
+
+    return(configlist) 
+} 
+
+#' Write SaaRclust configuration file
+#'
+#' Write a SaaRclust configuration file from a list structure.
+#'
+#' @param config A list structure with parameter values. Each entry will be written in one line.
+#' @param configfile Filename of the outputfile.
+#' @return \code{NULL}
+#' @author Aaron Taudt, David Porubsky
+#' @importFrom utils write.table
+
+writeConfig <- function(config, configfile) {
+
+    ## Printing function
+    formatstring <- function(string) {
+    if (is.character(string) & length(string)>1) {
+        string <- paste0("c('",paste0(string,collapse="','"),"')")
+    } else if (is.character(string) & length(string)==1) {
+        string <- paste0("'",string,"'")
+    } else if (is.numeric(string) & length(string)>1) {
+        string <- paste0("c(",paste0(string,collapse=','),")")
+    } else if (is.numeric(string) & length(string)==1) {
+        string <- string
+    } else if (is.null(string)) {
+        string <- "NULL"
+    }
+    return(string)
+    }
+    
+    f <- file(configfile, open='w')
+    cat("#============== SaaRclust configuration file ===============#\n", file=f)
+    # cat("\n[General]\n", file=f)
+    # for (i1 in c('numCPU','reuse.existing.files')) {
+    #     cat(i1," = ",formatstring(config[[i1]]),"\n", file=f)
+    # }
+    cat("\n[SaaRclust]\n", file=f)
+    for (i1 in c('min.contig.size','bin.size','store.data.obj','reuse.data.obj','num.clusters','alpha','best.prob','prob.th','ord.method','assembly.fasta','concat.fasta','z.limit','remove.always.WC')) {
+        cat(i1," = ",formatstring(config[[i1]]),"\n", file=f)
+    }
+    close(f, type='w')
+}
--- R/scaffoldDenovoAssembly.R
+++ R/scaffoldDenovoAssembly.R
@@ -5,6 +5,8 @@
 #' cluster/scaffold.
 #'
 #' @param outputfolder Folder to output the results. If it does not exist it will be created.
+#' @param configfile A file specifying the parameters of this function (without \code{bamfolder}, \code{outputfolder} and \code{configfile}). 
+#' If a \code{configfile} is specified, it will take priority over the command line parameters.
 #' @param min.contig.size A minimal contig size to be processed (default: 100kb).
 #' @param store.data.obj A logical indicating whether or not intermediate Rdata objects should be stored.
 #' @param reuse.data.obj A logical indicating whether or not existing files in \code{outputfolder} should be reused.
@@ -19,7 +21,7 @@
 #' @author David Porubsky
 #' @export
 #' 
-scaffoldDenovoAssembly <- function(bamfolder, outputfolder, min.contig.size=100000, bin.size=100000, store.data.obj=TRUE, reuse.data.obj=FALSE, num.clusters=100, alpha=0.1, best.prob=1, prob.th=0, ord.method='TSP', assembly.fasta=NULL, concat.fasta=TRUE, z.limit = 3, remove.always.WC = FALSE) {
+scaffoldDenovoAssembly <- function(bamfolder, outputfolder, configfile=NULL, min.contig.size=100000, bin.size=100000, store.data.obj=TRUE, reuse.data.obj=FALSE, num.clusters=100, alpha=0.1, best.prob=1, prob.th=0, ord.method='TSP', assembly.fasta=NULL, concat.fasta=TRUE, z.limit=3, remove.always.WC=FALSE) {
   ## Get total processing time
   ptm <- proc.time()
   
@@ -36,101 +38,124 @@ scaffoldDenovoAssembly <- function(bamfolder, outputfolder, min.contig.size=1000
   if (!file.exists(asmpath)) { dir.create(asmpath) }
   if (!file.exists(pltpath)) { dir.create(pltpath) }
   
-  ## TODO add config file
+  ## Get user defined config file
+  config <- NULL
+  if (is.character(configfile)) {
+    ## Read config file
+    errstring <- tryCatch({
+      config <- readConfig(configfile)
+      errstring <- ''
+    }, error = function(err) {
+      errstring <- paste0("Could not read configuration file ",configfile)
+    })
+    if (errstring!='') {
+      stop(errstring)
+    }
+  }
+  
+  ## Put all parameters into list and merge with config ##
+  params <- list(min.contig.size=min.contig.size, bin.size=bin.size, store.data.obj=store.data.obj, 
+                 reuse.data.obj=reuse.data.obj, num.clusters=num.clusters, alpha=alpha, 
+                 best.prob=best.prob, prob.th=prob.th, ord.method=ord.method, assembly.fasta=assembly.fasta, 
+                 concat.fasta=concat.fasta, z.limit=z.limit, remove.always.WC=remove.always.WC)
+  config <- c(config, params[setdiff(names(params), names(config))])
+  
+  ## Make a copy of the config file ##
+  writeConfig(config = config, configfile=file.path(outputfolder, 'SaaRclust.config'))
   
   ## Get contigs/scaffolds names and sizes
   bamFile <- list.files(bamfolder, pattern = ".bam$", full.names = TRUE)[1]
   file.header <- Rsamtools::scanBamHeader(bamFile)[[1]]
   chrom.lengths <- file.header$targets
   ## Keep only contigs/scaffolds >=100Kb
-  chrom.lengths <- chrom.lengths[chrom.lengths >= min.contig.size]
+  chrom.lengths <- chrom.lengths[chrom.lengths >= config[['min.contig.size']]]
   chroms.in.data <- names(chrom.lengths)
 
   ## Get counts per genomic regions ##
-  destination <- file.path(datapath, paste0("rawCounts_", bin.size,"bp_chunks.RData"))
-  if (reuse.data.obj) {
+  destination <- file.path(datapath, paste0("rawCounts_", config[['bin.size']],"bp_chunks.RData"))
+  if (config[['reuse.data.obj']]) {
     if (file.exists(destination)) {
       message("Loading previously generated BAM read counts ...\n", destination)
       counts.l <- get(load(destination))
     } else {
-      counts.l <- importBams(bamfolder = bamfolder, chromosomes = chroms.in.data, bin.size = bin.size)
+      counts.l <- importBams(bamfolder = bamfolder, chromosomes = chroms.in.data, bin.size = config[['bin.size']])
     }
   } else {
-    counts.l <- importBams(bamfolder = bamfolder, chromosomes = chroms.in.data, bin.size = bin.size)
+    counts.l <- importBams(bamfolder = bamfolder, chromosomes = chroms.in.data, bin.size = config[['bin.size']])
   }
   ## Store data object
-  if (store.data.obj) {
+  if (config[['store.data.obj']]) {
     save(counts.l, file = destination)
   }
 
   ## Perform hard clustering ##
-  destination <- file.path(datapath, paste0("hardClust_", num.clusters, "K_", bin.size,"bp_chunks.RData"))
-  if (reuse.data.obj) {
+  destination <- file.path(datapath, paste0("hardClust_", config[['num.clusters']], "K_", config[['bin.size']], "bp_chunks.RData"))
+  if (config[['reuse.data.obj']]) {
     if (file.exists(destination)) {
       message("Loading previously generated hard clustering results ...\n", destination)
       hardClust.ord <- get(load(destination))
     } else {
       set.seed(1000) ## to reproduce hard clustering results
-      hardClust.ord <- hardClust(counts.l, num.clusters=num.clusters, nstart = 1000)
+      hardClust.ord <- hardClust(counts.l, num.clusters=config[['num.clusters']], nstart = 1000)
     }
   } else {
     set.seed(1000) ## to reproduce hard clustering results
-    hardClust.ord <- hardClust(counts.l, num.clusters=num.clusters, nstart = 1000)
+    hardClust.ord <- hardClust(counts.l, num.clusters=config[['num.clusters']], nstart = 1000)
   }  
   ## Store data object
-  if (store.data.obj) {
+  if (config[['store.data.obj']]) {
     save(hardClust.ord, file = destination)
   }
   
   ## Estimate EM parameters
-  theta.estim <- estimateTheta(counts.l, hard.clust=hardClust.ord, alpha=alpha)
+  theta.estim <- estimateTheta(counts.l, hard.clust=hardClust.ord, alpha=config[['alpha']])
   ## Set theta parameter
   theta.param <- theta.estim
   ## Set pi parameter
   readsPerCluts <- table(hardClust.ord)
-  pi.param <- readsPerCluts/sum(readsPerCluts)
+  pi.param <- readsPerCluts / sum(readsPerCluts)
 
   ## RUN EM ##
-  destination <- file.path(datapath, paste0("softClust_", num.clusters, "K_", bin.size,"bp_chunks.RData"))
-  if (reuse.data.obj) {
+  destination <- file.path(datapath, paste0("softClust_", config[['num.clusters']], "K_", config[['bin.size']], "bp_chunks.RData"))
+  if (config[['reuse.data.obj']]) {
     if (file.exists(destination)) {
       message("Loading previously generated soft clustering results ...\n", destination)
       EM.obj <- get(load(destination))
     } else {
-      EM.obj <- EMclust(counts.l=counts.l, theta.param=theta.param, pi.param=pi.param, num.iter=20, alpha=alpha, logL.th=1, log.scale=TRUE)
+      EM.obj <- EMclust(counts.l, theta.param=theta.param, pi.param=pi.param, num.iter=20, alpha=config[['alpha']], logL.th=1, log.scale=TRUE)
     }
   } else {
-    EM.obj <- EMclust(counts.l=counts.l, theta.param=theta.param, pi.param=pi.param, num.iter=20, alpha=alpha, logL.th=1, log.scale=TRUE)
+    EM.obj <- EMclust(counts.l, theta.param=theta.param, pi.param=pi.param, num.iter=20, alpha=config[['alpha']], logL.th=1, log.scale=TRUE)
   }
   ## Store data object
-  if (store.data.obj) {
+  if (config[['store.data.obj']]) {
     save(EM.obj, file = destination)
   }
   
   ## Get cluster IDs that belong to the same chromosome/scaffold ##
-  split.pairs <- connectDividedClusters(theta.param = EM.obj$theta.param, z.limit = z.limit, remove.always.WC = remove.always.WC)
+  split.pairs <- connectDividedClusters(theta.param=EM.obj$theta.param, z.limit=config[['z.limit']], remove.always.WC=config[['remove.always.WC']])
   ## Store data object
-  destination <- file.path(datapath, paste0("connectedClusters_", bin.size,"bp_chunks.RData"))
-  if (store.data.obj) {
+  destination <- file.path(datapath, paste0("connectedClusters_", config[['bin.size']], "bp_chunks.RData"))
+  if (config[['store.data.obj']]) {
     save(split.pairs, file = destination)
   }
   
   ## Assign contigs to clusters based on soft probabilities ##
-  clustered.grl <- counts2ranges(counts.l=counts.l, saarclust.obj = EM.obj, best.prob = best.prob, prob.th = prob.th)
+  clustered.grl <- counts2ranges(counts.l, saarclust.obj=EM.obj, best.prob=config[['best.prob']], prob.th=config[['prob.th']])
   
   ## Order and orient contigs ##
-  destination <- file.path(asmpath, paste0("ordered&oriented_", bin.size,"bp_chunks.tsv"))
-  ordered.contigs.gr <- orderAndOrientClusters(clustered.grl = clustered.grl, split.pairs = split.pairs, ord.method = ord.method, alpha = alpha, bin.size = bin.size, filename = destination)
+  destination <- file.path(asmpath, paste0("ordered&oriented_", config[['bin.size']], "bp_chunks.tsv"))
+  ordered.contigs.gr <- orderAndOrientClusters(clustered.grl=clustered.grl, split.pairs=split.pairs, ord.method=config[['ord.method']], alpha=config[['alpha']], bin.size=config[['bin.size']], filename=destination)
   ## Store data object
-  destination <- file.path(asmpath, paste0("ordered&oriented_", bin.size,"bp_chunks.RData"))
+  destination <- file.path(asmpath, paste0("ordered&oriented_", config[['bin.size']], "bp_chunks.RData"))
   if (store.data.obj) {
     save(ordered.contigs.gr, file = destination)
   }
   
   ## Export clustered FASTA ##
-  if (!is.null(assembly.fasta) & is.character(assembly.fasta)) {
-    if (file.exists(assembly.fasta)) {
-      exportPseudoChromosomalScaffolds(clustered.gr = ordered.contigs.gr, assembly.fasta = assembly.fasta, outputfolder = asmpath, concat.fasta = concat.fasta)
+  if (!is.null(config[['assembly.fasta']]) & is.character(config[['assembly.fasta']])) {
+    if (file.exists(config[['assembly.fasta']])) {
+      exportPseudoChromosomalScaffolds(clustered.gr=ordered.contigs.gr, assembly.fasta=config[['assembly.fasta']], outputfolder=asmpath, concat.fasta=config[['concat.fasta']])
     } else {
       message("Submitted 'assembly.fasta' does not exists !!!")
     }  
