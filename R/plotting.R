b8aa0aa7e87a42ff9fdd166718c96cae53b2fd24
David Porubsky <david.porubsky@gmail.com>
2019-11-28 14:47:11 -0800

    Removed unused functions

--- R/plotting.R
+++ R/plotting.R
@@ -1,139 +1,3 @@
-#' Plot data quality measures.
-#'
-#' Takes imported data table and plots some relevant data quality measures.
-#'
-#' @param summary.tab Imported data table.
-#' @author David Porubsky
-#' @export
-
-plotQualMeasure <- function(summary.tab) {
-
-  mapp.dist.tab <- summary.tab$mapp.stat.counts
-  mapp.gaps.tab <- summary.tab$mapp.gaps.stat
-  SScov.stat.m <- summary.tab$SScov.stat          
-  ord <- order(match(rownames(SScov.stat.m), mapp.dist.tab$PBreadNames))
-  SScov.stat.m <- SScov.stat.m[ord,]
-  
-  #plotting distribution of SSreads mapped to PB reads
-  read.map.dist.mean <- round(mean(mapp.dist.tab$SSread.perPB))
-  read.dist.plt <- ggplot(mapp.dist.tab) + geom_line(aes(x=c(1:nrow(mapp.dist.tab)),y=SSread.perPB)) + theme_bw() + geom_hline(yintercept = read.map.dist.mean, color="red") + geom_text(aes(nrow(mapp.dist.tab), read.map.dist.mean, label = read.map.dist.mean, vjust = -1), color="red") + xlab("Sorted PB reads") + ylab("# of mapped SS reads")
-  read.dist.plt.log <- ggplot(mapp.dist.tab) + geom_line(aes(x=c(1:nrow(mapp.dist.tab)),y=SSread.perPB)) + theme_bw() + geom_hline(yintercept = read.map.dist.mean, color="red") + geom_text(aes(nrow(mapp.dist.tab), read.map.dist.mean, label = read.map.dist.mean, vjust = -1), color="red") + xlab("Sorted PB reads") + ylab("# of mapped SS reads (log10)") + scale_y_continuous(trans = "log10")
-  
-  #plotting distribution of SSlibs represented per PB read
-  SSlib.perPB.mean <- round(mean(mapp.dist.tab$SSlib.perPB))
-  SSlib.perPB.plt <- ggplot(mapp.dist.tab) + geom_line(aes(x=c(1:nrow(mapp.dist.tab)),y=SSlib.perPB)) + theme_bw() + geom_hline(yintercept = SSlib.perPB.mean, color="red") + geom_text(aes(nrow(mapp.dist.tab), SSlib.perPB.mean, label = SSlib.perPB.mean, vjust = -1), color="red") + xlab("PB reads sorted by the number\nof SSreads per PBread") + ylab("# of SS libs per PB read")
-  SSlib.perPB.plt.log <- ggplot(mapp.dist.tab) + geom_line(aes(x=c(1:nrow(mapp.dist.tab)),y=SSlib.perPB)) + theme_bw() + geom_hline(yintercept = SSlib.perPB.mean, color="red") + geom_text(aes(nrow(mapp.dist.tab), SSlib.perPB.mean, label = SSlib.perPB.mean, vjust = -1), color="red") + xlab("PB reads sorted by the number\nof SSreads per PBread") + ylab("# of SS libs per PB read (log10)") + scale_y_continuous(trans = "log10")
-  
-  #plotting sum of gaps per PB read normalized by SSread counts mapped to a given PB read
-  gaps.perPB.top1perc <- round(quantile(mapp.dist.tab$gaps.perPB.norm,prob=1-1/100)) #99th quantile
-  gaps.perPB.plt <- ggplot(mapp.dist.tab) + geom_line(aes(x=c(1:nrow(mapp.dist.tab)),y=gaps.perPB.norm)) + theme_bw() + geom_hline(yintercept = gaps.perPB.top1perc, color="red") + geom_text(aes(nrow(mapp.dist.tab), gaps.perPB.top1perc, label = gaps.perPB.top1perc, vjust = -1), color="red") + xlab("PB reads sorted by the number\nof SSreads per PBread") + ylab("Normalized sum of gaps per PB read")
-  
-  #plotting counts of SS reads per lib per PB read
-  mapp.SScov.stat.mean <- mean(SScov.stat.m[SScov.stat.m>0])
-  SScov.stat.m[SScov.stat.m==0] <- NA
-  SScov.stat.df <- data.frame(value=rowMeans(SScov.stat.m, na.rm = T))
-  SScov.stat.plt <- ggplot(SScov.stat.df) + geom_line(aes(x=c(1:nrow(SScov.stat.df)),y=value)) + theme_bw() + geom_hline(yintercept = mapp.SScov.stat.mean, color="red") + geom_text(aes(nrow(SScov.stat.df), mapp.SScov.stat.mean, label = mapp.SScov.stat.mean, vjust = -1), color="red") + xlab("PB reads sorted by the mean number\nof SSreads per SSlib per PBread") + ylab("Mean counts of SSreads per SSlib per PB read")
-  
-  #plotting match with gaps per SS read flagged TRUE or FALSE based on mapping agreement
-  gaps.perSS.mean <- round(mean(mapp.gaps.tab$matchWithgaps))
-  accur.counts <- table(mapp.gaps.tab$mapp.accur)
-  falses <- round((accur.counts[1]/sum(accur.counts))*100, digits = 2)
-  trues <- round((accur.counts[2]/sum(accur.counts))*100, digits = 2)
-  false.lab <- paste('MISS ', falses, '%', sep = "")
-  true.lab <- paste('MATCH ', trues, '%', sep = "")
-  gaps.perSS.plt <- ggplot(mapp.gaps.tab) + geom_linerange(aes(x=c(1:nrow(mapp.gaps.tab)),ymin=0, ymax=matchWithgaps, color=mapp.accur)) + theme_bw() + geom_hline(yintercept = gaps.perSS.mean, color="red") + geom_text(aes(nrow(mapp.gaps.tab), gaps.perSS.mean, label = gaps.perSS.mean, vjust = -1), color="red") + xlab("Sorted SS reads by the length\nof SS read alignment (bp)") + ylab("(bp) SS read alignment with gaps (log10)") + scale_y_continuous(trans = "log10") + scale_color_manual(labels = c(false.lab, true.lab), values = c("red", "green"))
- 
-  #plot PB read length distribution
-  PBreadLenDist <- ggplot(summary.tab$PBreadLenDist) + geom_linerange(aes(x=midpoints, ymin=0, ymax=freq), size=3)
-  
-  suppressWarnings( plt <- plot_grid(read.dist.plt, read.dist.plt.log, SSlib.perPB.plt, SSlib.perPB.plt.log, SScov.stat.plt, gaps.perPB.plt, PBreadLenDist, ncol = 2) )
-  return(plt)
-}
-
-
-#' Plot heatmap of responsibilities of each PB reads for each cluster as a probability value.
-#'
-#' @param pVal.df A \code{data.frame} of probability values.
-#' @param colOrder A \code{vector} of indices representing of cluster order on heatmap.
-#' @param num.clusters Number of cluster present in probability table.
-#' @author David Porubsky
-#' @export
-
-plotHeatmap <- function(pVal.df=NULL, colOrder=NULL, num.clusters=NULL) {
-
-  #order clusters based on most likely chromosome partners                               
-  if (!is.null(colOrder)) {                               
-    pVal.df[,1:length(colOrder)] <- pVal.df[,colOrder]
-    colnames(pVal.df)[1:num.clusters] <- colOrder
-  }
-  
-  if (!is.null(num.clusters)) {
-
-    chr.ids <- names(sort(table(pVal.df$PBchrom), decreasing = T))
-    chr.ids <- gsub('^chr', '', chr.ids)
-    #chr.ids <- sort(as.numeric(chr.ids))
-    chr.colors <- rep(c("gray48","gray72"), ceiling(length(chr.ids)/2))
-    chr.colors <- chr.colors[1:length(chr.ids)]
-    names(chr.colors) <- chr.ids
-    
-    pVal.df$PBchrom <- gsub('^chr', '', pVal.df$PBchrom)
-    pVal.df$PBchrom <- factor(pVal.df$PBchrom, levels=chr.ids)
-    pVal.df <- pVal.df[order(pVal.df$PBchrom),]
-    
-    #set unexpected directionality flags to 1
-    pVal.df$PBflag[pVal.df$PBflag != 0 & pVal.df$PBflag != 16] <- 1
-    
-    ha1 = rowAnnotation(df = data.frame(chr = pVal.df$PBchrom), col = list(chr=chr.colors))
-    ha2 = rowAnnotation(df = data.frame(PB.dir = pVal.df$PBflag), col = list(PB.dir = c('0'="chocolate1", '16'="chartreuse3", '1'="white")))
-    hm <- Heatmap(pVal.df[,c(1:num.clusters)], name = "Probs", cluster_columns = F, cluster_rows = F, show_row_names = FALSE)
-    hm + ha1 + ha2
-  } else {
-    message("num.clusters not specified!!!\n")
-  }  
-}  
-
-
-#' Plot accuracy of clustring in respect to known location of each PacBio read.
-#'
-#' @inheritParams plotHeatmap
-#' @param thresh A \code{vector} of probability thresholds to obtain accuracy of clustering. 
-#' @author David Porubsky
-#' @export
-
-plotClustAccuracy <- function(pVal.df=NULL, num.clusters=NULL, thresh=c(0.5,0.6,0.7,0.8,0.9,0.99)) {
-  pVals <- pVal.df[,c(1:num.clusters)]
-  chr.rows <- pVal.df$PBchrom
-  
-  acc.l <- list()
-  for (th in thresh) {
-    max.pVal <- apply(pVals, 1, max)
-    mask <- max.pVal >= th
-    ord <- apply(pVals[mask,], 1, which.max)
-    chr.clusts <- split(chr.rows[mask], ord)
-    chr.clusts <- lapply(chr.clusts, unlist) #Check why is this problem???
-    clust.acc <- getClusterAcc(chr.clusts)
-    frac.corr <- sum(clust.acc$stat$trues)/(sum(clust.acc$stat$trues) + sum(clust.acc$stat$miss))
-    eval.reads <- table(mask)
-    eval.reads <- eval.reads[2]/sum(eval.reads)
-    acc.l[[as.character(th)]] <- c(frac.corr, eval.reads)
-  } 
-  
-  #get hard clust accuracy
-  chr.clusts <- split(chr.rows, pVal.df$hardClust)
-  chr.clusts <- lapply(chr.clusts, unlist) #Check why is this problem???
-  clust.acc <- getClusterAcc(chr.clusts)
-  frac.corr <- sum(clust.acc$stat$trues)/(sum(clust.acc$stat$trues) + sum(clust.acc$stat$miss))
-  
-  m <- do.call(rbind, acc.l)
-  df <- data.frame(values=m[,1], eval=m[,2], thresh = factor(thresh, levels=rev(thresh)))
-  HC <- data.frame(values=frac.corr, eval=1, thresh='HardClust')
-  
-  plt <- ggplot(df) + geom_point(aes(x=values, y=eval, color=thresh), size=5) + geom_linerange(aes(ymin=-Inf, x=values, ymax=eval, color=thresh)) + scale_y_continuous(limits = c(0,1)) + scale_color_manual(values = brewer.pal(n=7, name="Set1"), name="Prob threshold") + ylab("(%) evaluated PB reads") + xlab("(%) correctly assigned PB reads")
-  plt <- plt + geom_point(data=HC, aes(x=values, y=eval, color=thresh), size=5, inherit.aes = F) + geom_linerange(data=HC, aes(ymin=-Inf, x=values, ymax=eval, color=thresh), inherit.aes = F)
-  return(plt)
-}
-
-
 #' Plot theta estimates resulting from EM algorithm.
 #'
 #' @param theta.param A \code{list} of estimated cell types for each cluster and each cell.
@@ -323,14 +187,16 @@ plotContigStrandStates <- function(contig.states = NULL, cluster.rows=FALSE, clu
 #' @param min.mapq Minimum mapping quality of a contig to the refrence sequence.
 #' @param bsgenome A \code{\link{GBSgenome-class}} object to provide chromosome lengths for plotting.
 #' @param blacklist A \code{\link{GRanges-class}} object of regions to be removed.
-#' @param report Plot either 'clustering' or 'ordering' of the contigs. Default: 'clustering'
+#' @param report Plot either 'clustering', 'ordering' or 'orienting' of the contigs. Default: 'clustering'.
+#' @param min.contig.size Minimal contigs size to plot.
+#' @param reverse.x Set to \code{TRUE} if x-axis should be horizontaly reversed.
 #' @return A \code{ggplot} object.
 #' @importFrom RColorBrewer brewer.pal.info brewer.pal
 #' @importFrom tidyr separate
 #' @author David Porubsky
 #' @export
 #' 
-plotClusteredContigs <- function(bedfile, min.mapq=10, bsgenome=NULL, blacklist=NULL, report='clustering') {
+plotClusteredContigs <- function(bedfile, min.mapq=10, bsgenome=NULL, blacklist=NULL, report='clustering', min.contig.size=NULL, reverse.x=FALSE) {
   ## Use standard chromosomes only
   chroms <- paste0('chr', c(1:22, 'X','Y'))
   ## Read-in mapped congtigs to the human reference genome
@@ -340,11 +206,22 @@ plotClusteredContigs <- function(bedfile, min.mapq=10, bsgenome=NULL, blacklist=
   if (min.mapq > 0) {
     data <- data[data$mapq >= min.mapq,]
   }
+  ## Filter by contig size
+  if (!is.null(min.contig.size)) {
+    if (min.contig.size > 0) {
+      mask <- (data$end - data$start) >= min.contig.size
+      data <- data[mask,]
+    }  
+  }
+  
   plt.df <- tidyr::separate(data, col = info, sep = '_', into = c('contig', 'order','cluster.ID'))
   plt.df$seqnames <- factor(plt.df$seqnames, levels=chroms)
   plt.df$order <- as.numeric(plt.df$order)
   ## Keep only standard chromosomes
   plt.df <- plt.df[plt.df$seqnames %in% chroms,]
+  
+  #plt.df$cluster.ID <- factor(plt.df$cluster.ID, levels = mixedsort(unique(plt.df$cluster.ID)))
+  
   ## Prepare ideogram plot
   seq.len <- GenomeInfoDb::seqlengths(bsgenome)[chroms]
   ideo.df <- data.frame(seqnames=names(seq.len), length=seq.len)
@@ -382,6 +259,16 @@ plotClusteredContigs <- function(bedfile, min.mapq=10, bsgenome=NULL, blacklist=
       theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank()) +
       theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
       theme(strip.text.y = element_text(angle = 180))
+  } else if (report == 'orienting') {
+    plt <- ggplot2::ggplot() + geom_rect(data = ideo.df, aes(xmin=0, xmax=length, ymin=0, ymax=1), fill=NA, color="black") +
+      facet_grid(seqnames ~ ., switch = 'y') +
+      geom_rect(data=plt.df, aes(xmin=start, xmax=end, ymin=0, ymax=1, fill=dir)) +
+      scale_fill_manual(values = c('chocolate1', 'cadetblue4')) +
+      scale_x_continuous(expand = c(0,0)) +
+      theme_void() +
+      theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank()) +
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      theme(strip.text.y = element_text(angle = 180))
   } else {
     message("Please choose to report either 'clustering' or 'ordering'!!!")
   }  
@@ -392,6 +279,12 @@ plotClusteredContigs <- function(bedfile, min.mapq=10, bsgenome=NULL, blacklist=
     blacklist.df <- blacklist.df[blacklist.df$seqnames %in% chroms,]
     plt <- plt + geom_rect(data=blacklist.df , aes(xmin=start, xmax=end, ymin=0, ymax=1), fill='white')
   }
+  
+  ## Reverse x-axis
+  if (reverse.x) {
+    plt <- plt + scale_x_reverse()
+  }
+  
   ## Return final plot
   return(plt)
 }
