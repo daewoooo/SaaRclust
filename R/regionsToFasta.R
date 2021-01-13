02a31f38a71893d8472d78019373ca8ac6275f58
David Porubsky <david.porubsky@gmail.com>
2021-01-13 12:43:01 -0800

    Adding new function to export genomic regions into FASTA file

---
+++ R/regionsToFasta.R
@@ -0,0 +1,68 @@
+#' Export FASTA sequences from a set of Genomic Ranges.
+#'
+#' This function takes a \code{\link{GRanges-class}} object and extracts a genomic sequence 
+#' from these regions either from an original range or a range expanded on each side by
+#' defined number of bases.
+#' 
+#' @param gr A \code{\link{GRanges-class}} object of genomic regions to extract genomic sequence from. 
+#' @param bsgenome A \code{\link{BSgenome-class}} object of reference genome to get the genomic sequence from. 
+#' @param asm.fasta An assembly FASTA file to extract DNA sequence determined by 'gr' parameter.
+#' @param expand Expand ends of each genomic region by this length (in bp).
+#' @param fasta.save A path to a filename where to store final FASTA file.
+#' @importFrom Rsamtools indexFa FaFile scanFa scanFaIndex
+#' @importFrom BSgenome getSeq
+#' @importFrom Biostrings writeXStringSet
+#' @author David Porubsky
+#' @export
+#'
+regions2FASTA <- function(gr, bsgenome=NULL, asm.fasta=NULL, expand=0, fasta.save=NULL) {
+  ## Load BSgenome object
+  if (class(bsgenome) != 'BSgenome') {
+    if (is.character(bsgenome)) {
+      bsgenome <- tryCatch({
+        suppressPackageStartupMessages(library(bsgenome, character.only=TRUE))
+        bsgenome <- eval(parse(text=bsgenome)) ## replacing string by object
+      }, error = function(e) {return(NULL)})
+    }
+  }
+  
+  if (!is.null(asm.fasta)) {	
+    ## Check if submitted fasta file is indexed
+    asm.fasta.idx <- paste0(asm.fasta, ".fai")
+    if (!file.exists(asm.fasta.idx)) {
+      fa.idx <- Rsamtools::indexFa(file = asm.fasta)
+    }
+  }
+  
+  ## Expand regions of interest by certain size (downstream and upstream)
+  if (expand > 0) {
+    gr.seqLen <- seqlengths(gr)[as.character(seqnames(gr))]
+    start(gr) <- pmax(1, start(gr) - expand)
+    end(gr) <- pmin(gr.seqLen, end(gr) + expand)
+  }
+  
+  if (!is.null(bsgenome)) {
+    ## Extract FASTA from BSgenome object
+    gr.seq <- BSgenome::getSeq(bsgenome, gr)
+    names(gr.seq) <- as.character(gr)
+  } else if (is.character(asm.fasta)) {
+    ## Extract FASTA from user defined FASTA file
+    fa.file <- open(Rsamtools::FaFile(asm.fasta))
+    ## Remove sequences not present in the FASTA index
+    fa.idx <- Rsamtools::scanFaIndex(fa.file)
+    gr <- suppressWarnings( subsetByOverlaps(gr, fa.idx) )
+    ## Read in contigs for a given cluster
+    gr.seq <- Rsamtools::scanFa(file = fa.file, param = gr, as = "DNAStringSet")
+    names(gr.seq) <- as.character(gr)
+  } else {
+    stop("Please set a 'bsgenome' or 'asm.fasta' parameter!!!")
+  }
+  
+  ## Write final FASTA
+  if (is.character(fasta.save)) {
+    Biostrings::writeXStringSet(x = gr.seq, filepath = fasta.save, format = 'fasta')
+  } else {
+    warning("Please speficify 'fasta.save' if you want to export FASTA into a file!!!")
+  }  
+  return(gr.seq)
+}
